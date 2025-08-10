d8941eb02723a77da78cb1642d49e78bada7d471
Marco Vergueira <vergueira.marco@gmail.com>
2025-08-10 15:16:39 +0100

    test

--- pkgs/by-name/cu/cups-brother-dcp375cw/package.nix
+++ pkgs/by-name/cu/cups-brother-dcp375cw/package.nix
@@ -1,107 +1,109 @@
 {
+  pkgsi686Linux,
   lib,
   stdenv,
   fetchurl,
-  cups,
   dpkg,
-  gnused,
   makeWrapper,
   ghostscript,
   file,
-  a2ps,
-  coreutils,
+  gnused,
   gnugrep,
+  coreutils,
   which,
-  gawk,
+  perl,
 }:
 let
-  version = "1.1.3-1";
+  version = "3.5.0-1";
   model = "dcp375cw";
-  cupsdeb = fetchurl {
-    url = "https://download.brother.com/welcome/dlf005429/dcp375cwcupswrapper-${version}.i386.deb";
-    hash = "sha256-ZgE2o/xU11+MzSnBYakXZE5m+Qa85/KIo31wKWAmsGY=";
-  };
-  lprdeb = fetchurl {
-    url = "https://download.brother.com/welcome/dlf005427/dcp375cwlpr-${version}.i386.deb";
-    hash = "sha256-ZgE2o/xU11+MzSnBYakXZE5m+Qa85/KIo31wKWAmsGY=";
-  };
+  interpreter = "${pkgsi686Linux.stdenv.cc.libc}/lib/ld-linux.so.2";
 in
 stdenv.mkDerivation {
   pname = "cups-brother-${model}";
   inherit version;
-
-  srcs = [
-    cupsdeb
-    lprdeb
-  ];
+  src = fetchurl {
+    url = "https://download.brother.com/welcome/dlf105465/mfcj6940dwpdrv-${version}.i386.deb";
+    hash = "sha256-kjwh8u4oX+Ae8fY4pQx7EvWUFVh3ClMxHIqJN/3krP8=";
+  };
 
   nativeBuildInputs = [
     dpkg
     makeWrapper
   ];
 
-  buildInputs = [
-    cups
-    ghostscript
-    a2ps
-    gawk
-  ];
-
   unpackPhase = ''
-    dpkg-deb -x ${lprdeb} $out
-    dpkg-deb -x ${cupsdeb} $out
+    runHook preUnpack
+
+    dpkg-deb -x $src $out
+
+    runHook postUnpack
   '';
 
   installPhase = ''
-    substituteInPlace $out/opt/brother/Printers/${model}/lpd/filter${model} \
-      --replace /opt "$out/opt"
+    runHook preInstall
+
+    substituteInPlace $out/opt/brother/Printers/${model}/lpd/filter_${model} \
+      --replace-fail /usr/bin/perl ${lib.getExe perl} \
+      --replace-fail "PRINTER =~" "PRINTER = \"${model}\"; #" \
+      --replace-fail "BR_PRT_PATH =~" "BR_PRT_PATH = \"$out/opt/brother/Printers/${model}/\"; #"
+
+    substituteInPlace $out/opt/brother/Printers/${model}/cupswrapper/brother_lpdwrapper_${model} \
+      --replace-fail /usr/bin/perl ${lib.getExe perl} \
+      --replace-fail "basedir =~ " "basedir = \"$out/opt/brother/Printers/${model}/\"; #" \
+      --replace-fail "PRINTER =~ " "PRINTER = \"${model}\"; #" \
+      --replace-fail "LPDCONFIGEXE=" "LPDCONFIGEXE=\"$out/usr/bin/brprintconf_\"; #"
+
+    patchelf --set-interpreter ${interpreter} $out/opt/brother/Printers/${model}/lpd/i686/br${model}filter
+    patchelf --set-interpreter ${interpreter} $out/opt/brother/Printers/${model}/lpd/i686/brprintconf_${model}
+
+    mkdir -p $out/lib/cups/filter $out/share/cups/model
+    ln -s $out/opt/brother/Printers/${model}/lpd/filter_${model} $out/lib/cups/filter/brlpdwrapper${model}
+    ln -s $out/opt/brother/Printers/${model}/cupswrapper/brother_lpdwrapper_${model} $out/lib/cups/filter/brother_lpdwrapper_${model}
+    ln -s $out/opt/brother/Printers/${model}/cupswrapper/brother_${model}_printer_en.ppd $out/share/cups/model/brother_${model}_printer_en.ppd
+
+    runHook postInstall
   '';
-    # substituteInPlace $out/opt/brother/Printers/${model}/inf/br${model}rc \
-    #   --replace "PaperType=Letter" "PaperType=A4"
 
-    # patchelf --set-interpreter $(cat $NIX_CC/nix-support/dynamic-linker) \
-    #   $out/opt/brother/Printers/${model}/lpd/br${model}filter
+  postFixup = ''
+    wrapProgram $out/opt/brother/Printers/${model}/lpd/filter_${model} \
+        --prefix PATH ":" ${
+          lib.makeBinPath [
+            ghostscript
+            file
+            gnused
+            gnugrep
+            coreutils
+            which
+          ]
+        }
+    wrapProgram $out/opt/brother/Printers/${model}/cupswrapper/brother_lpdwrapper_${model} \
+      --prefix PATH ":" ${
+        lib.makeBinPath [
+          gnugrep
+          coreutils
+        ]
+      }
+    wrapProgram $out/opt/brother/Printers/${model}/lpd/i686/brprintconf_${model} \
+      --set LD_PRELOAD "${pkgsi686Linux.libredirect}/lib/libredirect.so" \
+      --set NIX_REDIRECTS /opt=$out/opt
+    wrapProgram $out/opt/brother/Printers/${model}/lpd/i686/br${model}filter \
+      --set LD_PRELOAD "${pkgsi686Linux.libredirect}/lib/libredirect.so" \
+      --set NIX_REDIRECTS /opt=$out/opt
+  '';
 
-    # mkdir -p $out/lib/cups/filter/
-    # ln -s $out/opt/brother/Printers/${model}/lpd/filter${model} $out/lib/cups/filter/brlpdwrapper${model}
-
-    # wrapProgram $out/opt/brother/Printers/${model}/lpd/filter${model} \
-    #   --prefix PATH ":" ${
-    #     lib.makeBinPath [
-    #       gawk
-    #       ghostscript
-    #       a2ps
-    #       file
-    #       gnused
-    #       gnugrep
-    #       coreutils
-    #       which
-    #     ]
-    #   }
-
-    # for f in $out/opt/brother/Printers/${model}/cupswrapper/cupswrapper${model}; do
-    #   wrapProgram $f --prefix PATH : ${
-    #     lib.makeBinPath [
-    #       coreutils
-    #       ghostscript
-    #       gnugrep
-    #       gnused
-    #     ]
-    #   }
-    # done
-
-    # mkdir -p $out/share/cups/model
-    # ln -s $out/opt/brother/Printers/${model}/cupswrapper/brother_${model}_printer_en.ppd $out/share/cups/model/
-    
-    # runHook postInstall
-
-  meta = with lib; {
+  meta = {
     homepage = "https://www.brother.com/";
-    description = "Brother ${model} printer driver";
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
-    license = licenses.unfree;
-    platforms = platforms.linux;
-    downloadPage = "https://support.brother.com/g/b/downloadlist.aspx?c=gb&lang=en&prod=${model}_all&os=128";
-    maintainers = with maintainers; [ marcovergueira ];
+    downloadPage = "https://support.brother.com/g/b/downloadlist.aspx?c=eu_ot&lang=en&prod=${model}_us_eu_as&os=128";
+    description = "Brother MFC- printer driver";
+    license = with lib.licenses; [
+      unfreeRedistributable
+      gpl2Only
+    ];
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    platforms = [
+      "x86_64-linux"
+      "i686-linux"
+    ];
+    maintainers = with lib.maintainers; [ marcovergueira ];
   };
 }
