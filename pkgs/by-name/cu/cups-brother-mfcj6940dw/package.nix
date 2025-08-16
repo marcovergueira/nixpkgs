bb1816520e09542699e7699b7420a80fc7092db9
Marco Vergueira <vergueira.marco@gmail.com>
2025-08-16 12:32:51 +0100

    test

--- pkgs/by-name/cu/cups-brother-mfcj6940dw/package.nix
+++ pkgs/by-name/cu/cups-brother-mfcj6940dw/package.nix
@@ -51,7 +51,7 @@ stdenv.mkDerivation {
       --replace-fail /usr/bin/perl ${lib.getExe perl} \
       --replace-fail "PRINTER =~" "PRINTER = \"${model}\"; #" \
       --replace-fail "BR_PRT_PATH =~" "BR_PRT_PATH = \"$out/opt/brother/Printers/${model}/\"; #" \
-      --replace-fail "my \$BRCONV=sprintf (" "my \$BRCONV=\"$out/opt/brother/Printers/${model}/lpd/x86_64/br${model}filter\"; #"
+      --replace-fail "my \$BRCONV=sprintf (" "my \$BRCONV=\"$out/opt/brother/Printers/${model}/lpd/i686/br${model}filter\"; #"
 
     substituteInPlace $out/opt/brother/Printers/${model}/cupswrapper/brother_lpdwrapper_${model} \
       --replace-fail /usr/bin/perl ${lib.getExe perl} \
